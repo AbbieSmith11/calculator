f0f885baf6f66d3c7482210faa206af183f045b0
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-11 10:44:42 +0100

    Initial set up of calculator component and added it to app.tsx

---
+++ src/components/calculator/Calculator.tsx
@@ -0,0 +1,13 @@
+import Button from '../buttons/Button.tsx';
+
+export default function Calculator() {
+
+function handleClick() {
+ 
+}
+
+    return (
+        <Button label={3} onClick={handleClick}/>
+
+    )
+}

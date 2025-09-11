87b0be9391e68b7dc46c56fd905dc80e6ea96fe4
AbbieSmith11 <194799929+AbbieSmith11@users.noreply.github.com>
2025-09-11 12:11:13 +0100

    Added functionality in calculator file for all buttons

--- src/components/calculator/Calculator.tsx
+++ src/components/calculator/Calculator.tsx
@@ -1,13 +1,60 @@
-import Button from '../buttons/Button.tsx';
+import Button from "../buttons/Button.tsx";
+import { useState } from "react";
 
-export default function Calculator() {
+export default function Calculator(){
 
-function handleClick() {
- 
-}
+    // variable for calculator display
+  const [display, setDisplay] = useState<string>("");
 
-    return (
-        <Button label={3} onClick={handleClick}/>
+  // Handle button click
+  const handleClick = (value: string | number) => {
+    if (value === 'C') {
+        setDisplay('');
+        return;
+    }
+    if (value === '=') {
+        handleEquals();
+        return;
+    }
+    setDisplay(display + value.toString());
+  };
 
-    )
+  const handleEquals = () => {
+    try {
+        const result = new Function (`'use strict'; return (${display})`)();
+        setDisplay(result.toString());
+    }
+    catch {
+        setDisplay('Error')
+    }
+  }
+
+  return (
+    <section>
+      <div>
+        <p>{display || '0'}</p>
+      </div>
+      <div>
+        <Button label={1} onClick={handleClick} />
+        <Button label={2} onClick={handleClick} />
+        <Button label={3} onClick={handleClick} />
+        <Button label={4} onClick={handleClick} />
+        <Button label={5} onClick={handleClick} />
+        <Button label={6} onClick={handleClick} />
+        <Button label={7} onClick={handleClick} />
+        <Button label={8} onClick={handleClick} />
+        <Button label={9} onClick={handleClick} />
+      </div>
+      <div>
+        <Button label={"+"} onClick={handleClick}></Button>
+        <Button label={"-"} onClick={handleClick}></Button>
+        <Button label={"%"} onClick={handleClick}></Button>
+        <Button label={"*"} onClick={handleClick}></Button>
+        <Button label={"/"} onClick={handleClick}></Button>
+        <Button label={"."} onClick={handleClick}></Button>
+        <Button label={"C"} onClick={handleClick}></Button>
+        <Button label={"="} onClick={handleClick}></Button>
+      </div>
+    </section>
+  );
 }
